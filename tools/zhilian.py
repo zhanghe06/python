8900c571febad59850092f434d9c6469b8e9696d
Zhang He <zhang_he06@163.com>
2015-04-06 16:08:44 +0800

    智联简历抓取-获取最优简历

---
+++ tools/zhilian.py
@@ -0,0 +1,66 @@
+# encoding: utf-8
+__author__ = 'zhanghe'
+
+
+def select_best_resume(resume_list):
+    '''
+    根据相关字段权重取出最优简历
+    :param resume_list:
+    :return:
+    '''
+    url_list = {}
+    if resume_list is None:
+        # 如果简历为空
+        url_list['resume'] = None
+        return url_list
+
+    for item in resume_list:
+        if u'英文' in item:
+            resume_list.remove(item)
+
+    # 获取最优简历url
+    # 如果只有一个简历，直接返回
+    if len(resume_list) == 1:
+        url_list['resume'] = resume_list[0]['url']
+    else:
+        # 如果有多个简历，先取出各字段的最优值
+        # 完整度
+        integrity_range = []
+        for item in resume_list:
+            integrity_range.append(item['integrity'])
+        max_integrity = max(integrity_range)
+        # 更新日期
+        update_date_range = []
+        for item in resume_list:
+            update_date_range.append(item['update_date'])
+        max_update_date = max(update_date_range)
+        # 开放程度
+        openness_range = []
+        for item in resume_list:
+            openness_range.append(item['openness'])
+        if u'开放' in openness_range:
+            max_openness = u'开放'
+        elif u'委托给智联' in openness_range:
+            max_openness = u'委托给智联'
+        else:
+            max_openness = u'保密'
+        # 权重
+        weight_range = []
+        for item in resume_list:
+            item['weight'] = 0
+            # 将每条简历与最优值比较，如果存在，则权重自增
+            if item['integrity'] == max_integrity:
+                item['weight'] += 100
+            if item['update_date'] == max_update_date:
+                item['weight'] += 10
+            if item['openness'] == max_openness:
+                item['weight'] += 1
+            weight_range.append(item['weight'])
+        # 最大权重
+        max_weight = max(weight_range)
+
+        for item_max in resume_list:
+            # 查询简历列表中有最大权重的一条记录
+            if item_max['weight'] == max_weight:
+                url_list['resume'] = item_max['url']
+    return url_list
--- zhilian_new.py
+++ zhilian_new.py
@@ -5,6 +5,7 @@ import requests
 import json
 import logging
 from pyquery import PyQuery as pq
+from tools import zhilian
 
 logging.basicConfig(level=logging.DEBUG, filename='zhilian.log', filemode='w')
 
@@ -48,6 +49,7 @@ if 'window.location.href = "http://i.zhaopin.com"' in login_response.text:
     for row in pq(rows):
         j = 0
         item_list = []
+        item_dict = {}
         for item in pq(row).find('.email5'):
             item_text = pq(item).text()
             print "[%s-%s]" % (i, j) + item_text
@@ -56,115 +58,31 @@ if 'window.location.href = "http://i.zhaopin.com"' in login_response.text:
                 item_list.append(item_text)
             # Python哲学的一句话：只用一种方式解决问题，所以自增操作完全可以用i+=1完成，就不需要i++了。
             j += 1
+        # 提取简历完整度
         if len(item_list) == 4:
-            item_list[2] = item_list[2].split()[-1]
+            item_list[2] = item_list[2].split()[-1].rstrip('%')
         # 简历url添加进item_list
         url = pq(row).find('.email5 .iconHover_2').attr('href')
         item_list.append('http://my.zhaopin.com/myzhaopin/' + url)
         print json.dumps(item_list).decode('raw_unicode_escape')
         i += 1
-        resume_list.append(item_list)
+        # 将列表转为可读性强的字典
+        item_dict['update_date'] = item_list[0]
+        item_dict['language'] = item_list[1]
+        item_dict['integrity'] = item_list[2]
+        item_dict['openness'] = item_list[3]
+        item_dict['url'] = item_list[4]
+        resume_list.append(item_dict)
     print json.dumps(resume_list, indent=4).decode('raw_unicode_escape')
 
-# def select_best_resume(self):
-#     '选择一个最佳的简历'
-#     s = self.session
-#
-#     resume_list_url = 'http://my.zhaopin.com/myzhaopin/resume_list.asp'
-#
-#     res_resume_list = s.get(resume_list_url)
-#     # print res_resume_list.text
-#
-#     d = Q(res_resume_list.text).find('div[class="emailL_tab emailL_tab_bgcolor"]')
-#
-#     # 简历列表
-#     resume_list = []
-#     for row in d:
-#         resume_row = {
-#             'resumes_url': '',  # 简历url
-#             'wzd': '',  # 完整度
-#             'update_date': '',  # 更新时间
-#             'open_level': '',  # 公开程度
-#             'language': '',  # 语言
-#             'n': 0  # 深度
-#         }
-#         for i1 in Q(row).find('a[class="resumesIcon iconHover_2"]'):
-#             # print Q(i1).attr('href')
-#             resume_row['resumes_url'] = 'http://my.zhaopin.com/myzhaopin/' + Q(i1).attr('href')
-#         for i2 in Q(row).find('.wzd_right'):
-#             # print Q(i2).text()
-#             resume_row['wzd'] = Q(i2).text().rstrip('%')
-#         for i3 in Q(row).find('div[class="email5 tab9"]'):
-#             # print Q(i3).text()
-#             resume_row['update_date'] = Q(i3).text()
-#         for i4 in Q(row).find('a[class="openSetno"]'):
-#             # print Q(i4).text()
-#             resume_row['open_level'] = Q(i4).text()
-#         for i5 in Q(row).find('.select_title'):
-#             # print Q(i5).text()
-#             resume_row['language'] = Q(i5).text()
-#
-#         resume_list.append(resume_row)
-#     # print(resume_list)
-#     # exit()
-#     url_list = {}
-#     if resume_list == []:
-#         # 如果简历为空
-#         # return 'no_resume'
-#         url_list['resume'] = None
-#         return url_list
-#
-#     for item_en in resume_list:
-#         if item_en['language'] == u'英文':
-#             resume_list.remove(item_en)
-#     #print(resume_list)
-#
-#     # 获取最优简历url
-#     # 如果只有一个简历，直接返回
-#     if len(resume_list) == 1:
-#         url_list['resume'] = resume_list[0]['resumes_url']
-#     else:
-#         # 如果有多个简历，先取出各字段的最优值
-#         wzd_range = []
-#         for item in resume_list:
-#             wzd_range.append(item['wzd'])
-#             max_wzd = max(wzd_range)
-#
-#         update_date_range = []
-#         for item in resume_list:
-#             update_date_range.append(item['update_date'])
-#         max_update_date = max(update_date_range)
-#
-#         open_level_range = []
-#         for item in resume_list:
-#             open_level_range.append(item['open_level'])
-#         if u'开放' in open_level_range:
-#             max_open_level = u'开放'
-#         elif u'委托给智联' in open_level_range:
-#             max_open_level = u'委托给智联'
-#         else:
-#             max_open_level = u'保密'
-#
-#         n_range = []  # 深度范围
-#         for item_n in resume_list:
-#             # 将每条简历与最优值比较，如果存在，则深度自增
-#             if item_n['wzd'] == max_wzd:
-#                 item_n['n'] += 100
-#             if item_n['update_date'] == max_update_date:
-#                 item_n['n'] += 10
-#             if item_n['open_level'] == max_open_level:
-#                 item_n['n'] += 1
-#             #print item_n['n']
-#
-#             n_range.append(item_n['n'])
-#         # 最大深度
-#         max_n = max(n_range)
-#
-#         for item_max in resume_list:
-#             # 查询简历列表中有最大深度的一条记录
-#             if item_max['n'] == max_n:
-#                 #print item_max
-#                 url_list['resume'] = item_max['resumes_url']
-#                 #print url_list['resume']
-#
-#     return url_list
+    if resume_list is None:
+        print('简历不存在')
+        exit()
+    # 去除英文简历
+    for item in resume_list:
+        if item['language'] == u'英文':
+            resume_list.remove(item)
+    print json.dumps(resume_list, indent=4).decode('raw_unicode_escape')
+    # 根据PRD描述的规则获取最优简历
+    best_resume = zhilian.select_best_resume(resume_list)
+    print best_resume
