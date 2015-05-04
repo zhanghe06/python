5b81c416a95713f0d842aaf5e5bbf6d3d668eee4
zhanghe06 <zhang_he06@163.com>
2015-05-04 15:52:17 +0800

    邮件配置文件
    实际使用需要修改这里的配置

---
+++ mail_config.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+__author__ = 'zhanghe'
+
+# Email地址和口令:
+from_email_name = u'管理中心'
+from_email = 'xxxxxx@163.com'
+password = 'xxxxxx'
+# SMTP服务器地址:
+smtp_server = 'smtp.163.com'
+# 收件人地址（仅显示，可以不是实际地址）:
+to_email_name = u'尊敬的用户'
+to_email = 'xxxxxx@163.com'
