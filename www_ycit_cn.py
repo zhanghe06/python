c02dbe28335fe996702c93f3c9446ee219fb32d1
Zhang He <zhang_he06@163.com>
2015-06-09 22:10:45 +0800

    新增对协程的支持

--- www_ycit_cn.py
+++ www_ycit_cn.py
@@ -5,6 +5,10 @@ import requests
 from pyquery import PyQuery as Pq
 import re
 import json
+import time
+import gevent
+from gevent import monkey
+monkey.patch_all()
 
 
 root_url = 'http://www.ycit.cn/'  # 爬虫入口
@@ -62,7 +66,7 @@ def web_crawler_pq(url_node=None):
     基于PyQuery的网页爬虫
     """
     if url_node is None:
-        url_node = root_url
+        print '待抓取列表为空'
     response = requests.get(url_node)
     text_pq = Pq(response.text)
     tags = text_pq('html').find('a')
@@ -72,11 +76,15 @@ def web_crawler_pq(url_node=None):
             url = url_filter(url_join(url_pre, web_host), web_domain)
             if url is not None and url not in url_list and url not in url_visited_list:  # 去重
                 url_list.append(url.rstrip('/'))
-    print json.dumps(url_list, indent=4, ensure_ascii=False)
-    print len(url_list)
+    # print json.dumps(url_list, indent=4, ensure_ascii=False)
+    print "待访问节点：%s" % len(url_list)
     url_visited_list.append(url_node)
-    save(url_list, 'url_list.json')
-    save(url_visited_list, 'url_visited_list.json')
+    print "已访问节点：%s" % len(url_visited_list)
+    end_time = time.time()
+    print "耗时：%0.2f S" % (end_time - start_time)
+    print '--------------'
+    # save(url_list, 'url_list.json')
+    # save(url_visited_list, 'url_visited_list.json')
 
 
 def web_crawler_re(url_node=None):
@@ -84,7 +92,7 @@ def web_crawler_re(url_node=None):
     基于正则表达式的网页爬虫
     """
     if url_node is None:
-        url_node = root_url
+        print '待抓取列表为空'
     response = requests.get(url_node)
     html = response.text
     reg = '<a .*?href="(.+?)".*?>'
@@ -94,21 +102,49 @@ def web_crawler_re(url_node=None):
             url = url_filter(url_join(tag, web_host), web_domain)
             if url is not None and url not in url_list and url not in url_visited_list:  # 去重
                 url_list.append(url.rstrip('/'))
-    print json.dumps(url_list, indent=4, ensure_ascii=False)
-    print len(url_list)
+    # print json.dumps(url_list, indent=4, ensure_ascii=False)
+    print "待访问节点：%s" % len(url_list)
     url_visited_list.append(url_node)
-    save(url_list, 'url_list.json')
-    save(url_visited_list, 'url_visited_list.json')
+    print "已访问节点：%s" % len(url_visited_list)
+    end_time = time.time()
+    print "耗时：%0.2f S" % (end_time - start_time)
+    print '--------------'
+    # save(url_list, 'url_list.json')
+    # save(url_visited_list, 'url_visited_list.json')
 
 
 if __name__ == "__main__":
-    while len(url_list) > 0:
+    start_time = time.time()
+    # while len(url_list) > 0:
         # web_crawler_pq(url_list.pop(0))  # PyQuery方式
-        web_crawler_re(url_list.pop(0))  # 正则方式
+        # web_crawler_re(url_list.pop(0))  # 正则方式
+
+    # 基于协程的抓取
+    while len(url_list) > 0:
+        threads = [gevent.spawn(web_crawler_re, i) for i in url_list]
+        gevent.joinall(threads)
+    print '结束'
 
 
 """
 查看测试结果：
 $ tail -f ~/code/python/static/url_list/url_list.json
 $ tail -f ~/code/python/static/url_list/url_visited_list.json
+
+相同耗时下抓取速度比较
+
+不使用协程：
+--------------
+待访问节点：539
+已访问节点：24
+耗时：42.85 S
+--------------
+
+使用协程：
+--------------
+待访问节点：1404
+已访问节点：312
+耗时：42.67 S
+--------------
+
 """
