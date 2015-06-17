f98e44ce09e185a13f2a28a7a1b05222fe7264c4
Zhang He <zhang_he06@163.com>
2015-06-18 00:41:16 +0800

    完善基金数据分析

--- fund_analysis.py
+++ fund_analysis.py
@@ -4,12 +4,16 @@ __author__ = 'zhanghe'
 import requests
 import time
 import json
+import os
 
 
+# app接口
 url = 'http://fundex2.eastmoney.com/FundWebServices/MyFavorInformation.aspx?t=kf&s=desc&sc=&pstart=0&psize=10000'
 
+# 待处理基金编号
 fund_list = ['100056', '161024']
 
+# 基金字典对应关系
 fund_dict = {
     'gztime': u'估值时间',
     'shzt': u'赎回状态',
@@ -30,50 +34,105 @@ fund_dict = {
     'isbuy': '1'
 }
 
+# 组装接口
 fc = ','.join(fund_list)
-
 url += '&fc=' + fc
 
-read_list = json.loads(requests.get(url).text)
+
+def timed_task(func, interval=60, start_time='2015-06-18 09:00:00', end_time='2015-06-18 15:00:00'):
+    """
+    循环定时任务
+    :param func:
+    :param interval:
+    :return:
+    """
+    run_time = time.mktime(time.strptime(start_time, '%Y-%m-%d %H:%M:%S'))
+    exit_time = time.mktime(time.strptime(end_time, '%Y-%m-%d %H:%M:%S'))
+    rest_time = run_time - time.time()  # 启动剩余时间
+    if rest_time > 0:
+        print '程序等待执行'
+    while True:
+        while run_time <= time.time():
+            func()
+            time.sleep(interval)  # 等待循环
+            if exit_time <= time.time():
+                print '程序执行完毕'
+                break
 
 
 def save_json(read_dict):
-    # 保存json至服务器
-    import os
+    """
+    保存json文件至服务器
+    :param read_dict:
+    :return:
+    """
     file_path = 'static/json/'
     if not os.path.isdir(file_path):
         os.mkdir(file_path)
-    filename = 'static/json/' + read_dict[u'基金编号'] + '.json'
-    result_json = json.dumps(read_dict, ensure_ascii=False, indent=0) + '\n'
+    filename = file_path + read_dict[u'基金编号'] + '.json'
+    result_json = json.dumps(read_dict, ensure_ascii=False) + '\n'  # 单行存储
     with open(filename, 'a') as f:
         f.write(result_json.encode('utf-8'))
 
 
-def run():
-    for i in read_list:
-        item = {}
-        # 基本信息
-        item[u'记录时间'] = time.strftime('%H:%M')
-        item[fund_dict['fundcode']] = i['fundcode']  # 基金编号
-        item[fund_dict['name']] = i['name']  # 基金名称
-        item[fund_dict['jjlx']] = i['jjlx']  # 基金类型
-        # 估值信息
-        item[fund_dict['gztime']] = i['gztime']  # 估值时间
-        item[fund_dict['gsz']] = i['gsz']  # 估算净值
-        item[fund_dict['gszzl']] = i['gszzl']  # 估算涨幅
-        # 历史信息
-        item[fund_dict['jzrq']] = i['jzrq']  # 净值日期
-        item[fund_dict['dwjz']] = i['dwjz']  # 单位净值
-        item[fund_dict['rzzl']] = i['rzzl']  # 日增长率
-        print json.dumps(item, ensure_ascii=False, indent=4)
-        save_json(item)
+def read_line(code):
+    """
+    逐行读取数据
+    :param code:
+    :return:
+    """
+    filename = 'static/json/' + code + '.json'
+    with open(filename, 'r') as files:
+        for each_line in files:
+            print each_line
+
+
+def run_read():
+    """
+    读取本地数据
+    """
+    for i in fund_list:
+        read_line(i)
+
+
+def run_write():
+    """
+    获取数据，写入本地
+    """
+    response = requests.get(url)
+    content = response.text
+    read_list = json.loads(content)
+    for item in read_list:
+        item_dict = {
+            # 基本信息
+            u'记录时间': time.strftime('%H:%M'),
+            fund_dict['fundcode']: item['fundcode'],
+            fund_dict['name']: item['name'],
+            fund_dict['jjlx']: item['jjlx'],
+            # 估值信息
+            fund_dict['gztime']: item['gztime'],
+            fund_dict['gsz']: item['gsz'],
+            fund_dict['gszzl']: item['gszzl'],
+            # 历史信息
+            fund_dict['jzrq']: item['jzrq'],
+            fund_dict['dwjz']: item['dwjz'],
+            fund_dict['rzzl']: item['rzzl']
+        }
+        print json.dumps(item_dict, ensure_ascii=False, indent=4)
+        save_json(item_dict)
 
 
 if __name__ == "__main__":
-    run()
+    # run_write()
+    # run_read()
+    timed_task(run_write, 5)
 
 
 """
+因为只有app接口数据实时更新
+这里采用app接口作为数据来源
+[使用Fiddler捕获接口即可]
+
 fc=100056,161024
 9点-16点
 整个过程9小时
@@ -81,13 +140,6 @@ fc=100056,161024
 9*60/2 = 270次/天
 
 # 100056.json 名称为基金编号
-analysis_list = []
 
 每2分钟 追加写入
-{u'编号': xxx, u'名称': xxx, u'时间': xxx, u'估值': xxx},
-{u'编号': xxx, u'名称': xxx, u'时间': xxx, u'估值': xxx},
-{u'编号': xxx, u'名称': xxx, u'时间': xxx, u'估值': xxx},
-{u'编号': xxx, u'名称': xxx, u'时间': xxx, u'估值': xxx},
-{u'编号': xxx, u'名称': xxx, u'时间': xxx, u'估值': xxx},
-{u'编号': xxx, u'名称': xxx, u'时间': xxx, u'估值': xxx},
 """
