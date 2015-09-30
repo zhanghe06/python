9472a0014060aa14410a21cafa20fae850cc0bc2
Zhang He <zhang_he06@163.com>
2015-09-30 09:28:29 +0800

    新增正则表达式提取email测试

--- test/test_re.py
+++ test/test_re.py
@@ -56,5 +56,45 @@ def re_html(html):
     return json.dumps(html_dict, ensure_ascii=False, indent=4)
 
 
+def get_email(html=None):
+    """
+    从文本中提取email
+    """
+    if html is None:
+        return []
+    email_rule = r'[^\_\@\s\W][\w\_\-\.]{1,}\@(?:[^\s\.]{1,}\.){1,}(?:[a-z]{2,4}\.?){1,2}'
+    email_list = re.compile(email_rule, re.S).findall(html)
+    # print json.dumps(email_list, ensure_ascii=False, indent=4)
+    # email_list_new = []
+    # for item in email_list:
+    #     email_list_new.append(item.lower())
+    # return email_list_new
+    return [item.lower() for item in email_list]
+
+
 if __name__ == '__main__':
-    print re_html(test_html)
+    # print re_html(test_html)
+
+    html_test = '''邮箱：yy_y.it@gabc-xx.com +zhang_he06@163.com .zhanG_he06@163.com -zhang_he06@163.com _zhang_he06@163.com @zhang_he06@163.com @zhang-he06@163.com zhanghe@baidu.com 地址：上海市'''
+    email_result = get_email(html_test)
+    print json.dumps(email_result, ensure_ascii=False, indent=4)
+
+"""
+测试结果
+[
+    "yy_y.it@gabc-xx.com",
+    "zhang_he06@163.com",
+    "zhang_he06@163.com",
+    "zhang_he06@163.com",
+    "zhang_he06@163.com",
+    "zhang_he06@163.com",
+    "zhang-he06@163.com",
+    "zhanghe@baidu.com"
+]
+"""
+
+"""
+(?:pattern)
+匹配 pattern 但不获取匹配结果
+例如， 'industr(?:y|ies) 就是一个比 'industry|industries' 更简略的表达式。
+"""
