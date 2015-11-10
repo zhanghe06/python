175a146d5cdb7f10b67846d0cfd3da5e2f459366
Zhang He <zhang_he06@163.com>
2015-11-10 09:35:38 +0800

    新增导出json文件工具类

--- tools/export.py
+++ tools/export.py
@@ -34,9 +34,34 @@ class ExportBulk(object):
         self.bulk_fp.close()
 
 
-def test():
+class ExportJson(object):
     """
-    测试
+    导出json文件工具类
+    """
+    def __init__(self, file_name):
+        if file_name is None:
+            file_name = 'json_%s.json' % time.time()
+        file_path = os.path.dirname(file_name)
+        if not os.path.isdir(file_path):
+            os.mkdir(file_path)
+        self.json_fp = open(file_name, 'a')
+
+    def write(self, data):
+        """
+        文件写入
+        """
+        self.json_fp.write(json.dumps(data).decode('raw_unicode_escape')+"\n")
+
+    def close(self):
+        """
+        关闭文件资源
+        """
+        self.json_fp.close()
+
+
+def test_bulk():
+    """
+    测试ExportBulk
     """
     export_bulk = ExportBulk('service', 'provider')
     test_id = '1'
@@ -46,4 +71,4 @@ def test():
 
 
 if __name__ == '__main__':
-    test()
+    test_bulk()
