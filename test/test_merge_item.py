447ee988086d7114f3176bbbe1d507063f426141
Zhang He <zhang_he06@163.com>
2015-11-16 18:19:15 +0800

    新增求两个字典的乘积和

--- test/test_merge_item.py
+++ test/test_merge_item.py
@@ -14,3 +14,16 @@ print ','.join([i for i in gg.values()])
 print ','.join([i['name'] for i in ff])
 
 print '第一条；第二条；第三条'.count('；')  # 分号的个数
+
+
+# 求两个字典的乘积和
+dict_a = {'a': 40, 'b': 20}
+dict_b = {'a': 30, 'b': 50}
+# total = 0
+# for x, y in dict_a.items():
+#     for p, q in dict_b.items():
+#         if x == p:
+#             total += y*q
+# print total
+
+print sum(y*n for x, y in dict_a.items() for m, n in dict_b.items() if x == m)
