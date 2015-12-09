86a82141dfef687d77ba133cbc653272c0a43160
Zhang He <zhang_he06@163.com>
2015-12-09 10:32:39 +0800

    新增selenium测试

---
+++ test/test_selenium.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+__author__ = 'zhanghe'
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
