8440b70f906f44893f5526713286fd38fe33970f
Zhang He <zhang_he06@163.com>
2015-12-11 14:11:55 +0800

    新增通过字典实现批量替换

--- tools/html.py
+++ tools/html.py
@@ -18,6 +18,15 @@ def replace_html(input_html, reg_expression=r'', replace_text=''):
     return output_html
 
 
+def replace_all(input_html, replace_dict):
+    """
+    用字典实现批量替换
+    """
+    for k, v in replace_dict.iteritems():
+        input_html = input_html.replace(k, v)
+    return input_html
+
+
 def strip_html(input_html):
     """
     去除html标签
@@ -153,6 +162,17 @@ def test_replace_char_entity():
     print replace_char_entity(html_test)
 
 
+def test_replace_all():
+    """
+    测试用字典实现批量替换
+    """
+    replacements = {'\\n': '', '\\r': ''}
+    print replace_all('ffff\\n\\r\\ngggg\\ndfdf', replacements)
+
+    replacements = {'\n': '', '\r': ''}
+    print replace_all('ffff\n\r\ngggg\ndfdf', replacements)
+
+
 if __name__ == '__main__':
     test_html = '''<h2>多云</h2>  '''
     print replace_html(test_html)
@@ -175,6 +195,7 @@ if __name__ == '__main__':
     html = '''<div style="padding-bottom:30px;">好消息：有手机&nbsp;就可以做的好<br>兼职 空余</BR>时间生<p>活工作两<P>不误！！！！<br><br>郑重声明：我们是公司直招，不是中介，不收任何费用，有收费情况请投诉、<br>非常期待您成为我们的伙伴，加不加入没关系，了解一下也没有关系<br><br>有意者可直接联系我们的企业客服QQ:848304882【在线咨询】 承诺不收取任何费用<br><br>我们不需要用太华丽的语言去宣称自己，我们只想叫我们的客户每天都有固定的收入，<br>并且我们一直在努力。我们只需要你每天有一定的上网时间，有部分的电脑知识，无经验公司可以免费培训。<br>---------------------------------------------------------------------<br><br>有意者可直接联系我们的企业客服QQ:848304882【在线咨询】 承诺不收取任何费用<br>工作要求：<br>1，工作认真，有上进心，有团队精神，服从工作安排。<br>2，有具备手机（3G网络）或 电脑，每天有1小时的空余时间。<br>3、普通话标准、 语言沟通能力强、 有简单的评判能力；<br>4、态度积极，性格开朗，做事有激情，抗压能力强，有责任心，执行力强；<br>5、要有团队合作精神；<br>6、有相关工作经验者优先；<br>7，无不良嗜好，为人心态良好，逻辑思维及条理清晰 有较强责任心。<br>8，小学以上学历，电脑操作基础懂，会打字聊天。<br>---------------------------------------------------------------------<br>有意者可直接联系我们的企业客服QQ:848304882【在线咨询】承诺不收取任何费用<br>来应聘的有志者请联系客服QQ:848304882  （注：为节省工作资源不收取简历，请联系企业客服，谢谢合作。）</div>'''
     print filter_tags(html)
     test_replace_char_entity()
+    test_replace_all()
 
 
 '''
