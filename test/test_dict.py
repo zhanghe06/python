1c4aa69535f47ba2729979b1729106ee554473e1
Zhang He <zhang_he06@163.com>
2016-08-20 23:22:06 +0800

    更新字典转换测试

--- test/test_dict.py
+++ test/test_dict.py
@@ -5,6 +5,10 @@
 __author__ = 'zhanghe'
 
 
+import itertools
+import json
+
+
 def test_01():
     """
     key value 数量相等
@@ -38,10 +42,64 @@ def test_03():
     print dict(zip(list_a[::-1], list_b))
 
 
+def test_group():
+    """
+    测试分组
+    """
+    a = [
+        {
+            "update_time": "2016-08-17 13:51:54",
+            "name": "尺寸",
+            "value": "小",
+            "create_time": "2016-08-17 13:51:54",
+            "id": 1,
+            "product_id": 1
+        },
+        {
+            "update_time": "2016-08-17 13:51:54",
+            "name": "尺寸",
+            "value": "大",
+            "create_time": "2016-08-17 13:51:54",
+            "id": 2,
+            "product_id": 1
+        },
+        {
+            "update_time": "2016-08-17 13:51:54",
+            "name": "颜色",
+            "value": "蓝",
+            "create_time": "2016-08-17 13:51:54",
+            "id": 3,
+            "product_id": 1
+        },
+        {
+            "update_time": "2016-08-17 13:51:54",
+            "name": "颜色",
+            "value": "绿",
+            "create_time": "2016-08-17 13:51:54",
+            "id": 4,
+            "product_id": 1
+        },
+        {
+            "update_time": "2016-08-17 13:51:55",
+            "name": "颜色",
+            "value": "红",
+            "create_time": "2016-08-17 13:51:55",
+            "id": 5,
+            "product_id": 1
+        }
+    ]
+
+    result = dict([(g, [i['value'] for i in list(k)]) for g, k in itertools.groupby(a, lambda x: x['name'])])
+    print json.dumps(result, indent=4, ensure_ascii=False)
+    result = [({'name': g, 'value': [i['value'] for i in list(k)]}) for g, k in itertools.groupby(a, lambda x: x['name'])]
+    print json.dumps(result, indent=4, ensure_ascii=False)
+
+
 if __name__ == '__main__':
     test_01()
     test_02()
     test_03()
+    test_group()
 
 
 """
@@ -52,4 +110,32 @@ if __name__ == '__main__':
 {'a': '30', 'c': '10', 'b': '20'}
 {'a': '10', 'c': '30', 'b': '20'}
 {'c': '20', 'b': '30', 'd': '10'}
+{
+    "尺寸": [
+        "小",
+        "大"
+    ],
+    "颜色": [
+        "蓝",
+        "绿",
+        "红"
+    ]
+}
+[
+    {
+        "name": "尺寸",
+        "value": [
+            "小",
+            "大"
+        ]
+    },
+    {
+        "name": "颜色",
+        "value": [
+            "蓝",
+            "绿",
+            "红"
+        ]
+    }
+]
 """
