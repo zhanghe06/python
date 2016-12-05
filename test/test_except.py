5808d35447870a38166ef48edc88adbeefb27699
Zhang He <zhang_he06@163.com>
2016-12-05 11:06:37 +0800

    更新异常测试

--- test/test_except.py
+++ test/test_except.py
@@ -8,13 +8,30 @@
 @time: 16-4-13 下午6:38
 """
 
+import time
+import traceback
 
 try:
     # raise Exception('error_message')
     raise Exception('error_message', 'error_code')
 except Exception as e:
+    # 打印异常
+    print type(e), e
+
+    time.sleep(0.1)
+
+    # 打印异常消息
     print type(e.message), e.message
-    print type(e.args[0]), e.args[0]
+
+    time.sleep(0.1)
+
+    # 打印异常参数
+    print e.args, type(e.args[0]), e.args[0]
+
+    time.sleep(0.1)
+
+    # 打印异常堆栈跟踪信息 stack traceback
+    traceback.print_exc()
 
 
 if __name__ == '__main__':
