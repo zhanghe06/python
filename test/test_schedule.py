a810f14186a629b3a9ba9d4a373dcf8afb9cebb7
Zhang He <zhang_he06@163.com>
2017-04-26 13:32:39 +0800

    新增Schedule调度策略测试

--- test/test_schedule.py
+++ test/test_schedule.py
@@ -15,14 +15,18 @@ import time
 
 def job():
     print("I'm working...")
+    print time.strftime('%Y-%m-%d %H:%M:%S')
+    time.sleep(80)
 
 
 def run():
-    schedule.every(10).minutes.do(job)
-    schedule.every().hour.do(job)
-    schedule.every().day.at("10:30").do(job)
-    schedule.every().monday.do(job)
-    schedule.every().wednesday.at("13:15").do(job)
+    # schedule.every(10).minutes.do(job)
+    # schedule.every().hour.do(job)
+    # schedule.every().day.at("10:30").do(job)
+    # schedule.every().monday.do(job)
+    # schedule.every().wednesday.at("13:15").do(job)
+    schedule.every().day.at("15:14").do(job)
+    schedule.every().day.at("15:15").do(job)
 
     while True:
         schedule.run_pending()
@@ -41,4 +45,14 @@ python 版本的定时调度
 schedule.every().hour.do(job)
 调度器开始工作后1个小时开始执行 job
 
+测试执行顺序
+设置任务处理时间超过调度间隔：
+schedule.every().day.at("15:14").do(job)
+schedule.every().day.at("15:15").do(job)
+结果：
+I'm working...
+2017-04-13 15:14:00
+I'm working...
+2017-04-13 15:15:21
+可以看出schedule任务调度是顺序执行，不是并行
 """
