7d24047c37fb1f68e1ace544957d9680f3a76617
Zhang He <zhang_he06@163.com>
2017-09-06 14:39:45 +0800

    更新文件测试说明

--- test/test_file.py
+++ test/test_file.py
@@ -37,3 +37,20 @@ for file_name in [item for item in os.listdir(root_dir) if item.startswith('resu
 
 if __name__ == '__main__':
     pass
+
+
+"""
+# python 打开文件的模式
+r 以只读模式打开文件
+w  以只写模式打开文件，且先把文件内容清空（truncate the file first）wb 以二进制格式打开一个文件只用于写入。如果该文件已存在则将其覆盖。如果该文件不存在，创建新文件。
+a   以添加模式打开文件，写文件的时候总是写到文件末尾，用seek也无用。打开的文件也是不能读的
+r+  以读写方式打开文件，文件可读可写，可写到文件的任何位置
+w+ 和r+不同的是，它会truncate the file first
+a+ 和r+不同的是，它只能写到文件末尾
+
+# 文件对象的方法
+file.closed        返回true如果文件已被关闭，否则返回false。
+file.mode        返回被打开文件的访问模式。
+file.name        返回文件的名称。
+file.softspace        如果用print输出后，必须跟一个空格符，则返回false。否则返回true。
+"""
